142231651dc24b26c2fa20db822ccc0a6be30cb3
Deepika Kala <49941789+deepika1998kala@users.noreply.github.com>
2024-08-28 13:39:26 +0530

    Time: 45 ms (58.94%), Space: 17.7 MB (37.20%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-
+/*
+    //Using the Better approach than the Brute Force approach 
     bool binarySearch(vector<int>& matrix, int target) {
         int left = 0;
         int right = matrix.size()-1;
@@ -17,12 +18,35 @@ public:
         }
         return false;
     }
+*/
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
+        /*int m = matrix.size();
         for(int i=0;i<m;i++){
             if(binarySearch(matrix[i], target)==true){
                 return true;
             }           
+        }
+        return false;*/
+
+        //Optimal Approach 
+        //Here we can use any corner 
+        // i) Top - Right 
+        // ii) Bottom - Left 
+        //But You can not use the other 2 corners
+        //Here I am Using the i)
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int row = 0;
+        int col = n-1;
+        while(row<m && col>=0){
+            if(matrix[row][col]<target){
+                row++;
+            }
+            else if(matrix[row][col]>target){
+                col--;
+            }else{
+                return true;
+            }
         }
         return false;
     }
